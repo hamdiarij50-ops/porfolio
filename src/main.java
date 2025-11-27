82a60dfc5124807a1a9aa2321638ce81bb2badc9
hamdiarij50-ops <hamdiarij50@gmail.com>
2025-11-27 19:25:34 +0100

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[]args) {
+        produit p=new produit (1,"test", marque: "marc",prix 100);
+        p.setdate(10,02,2005);
+        p.afficher();
+        System.out.println("date": "+p.getdate());}
+    }
+
